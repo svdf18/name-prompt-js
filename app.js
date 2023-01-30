3a52a11f63aaa031a7f0469df57d45494a1a8d6c
svdf18 <simonvandeurs@hotmail.com>
2023-01-30 21:23:19 +0100

    lil note
    lil note

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-document.body.innerHTML += "Hello JavaScript!";
-// alert("Hello Dat.js");
-console.log("Hello Dat.js");
-console.log("Hi Students!");
-console.log("This is your first JavaScript App 🎉");
+let person = prompt("Please enter your name");
+
+if (person != null) {
+    document.body.innerHTML = ("Hello " + person + "! How are you today?");
+}
+
+// trying to make a randomized, wholesome return consisting of two variables, one before and one after the prompt.
+
+// let person = prompt("Please enter your name");
+
+// if (person != null) {
+
+//     var befores = ['Hi', 'Hello', 'Buongiorno', 'Greetings'];
+//     var afters = ['! How are you today?', '! You are blessed and highly favoured', '! Let your kind light shine', '! This is your time for rest and relaxation']
+//     var before = befores[Math.floor(Math.random()*before.length)];
+//     var after = afters[Math.floor(Math.random()*after.length)];
+
+// document.body.innerHTML = (before + person + after);
+// }
+
+
