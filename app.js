eadf9fa417db7bb1c5ea228e6903c76ad0ddca32
svdf18 <simonvandeurs@hotmail.com>
2023-01-30 19:40:46 +0100

    lil ændring
    den mindste

---
+++ app.js
@@ -0,0 +1,5 @@
+document.body.innerHTML += "Hello JavaScript!";
+// alert("Hello Dat.js");
+console.log("Hello Dat.js");
+console.log("Hi Students!");
+console.log("This is your first JavaScript App 🎉");
