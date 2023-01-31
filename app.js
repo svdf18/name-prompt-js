c444660632d3d1edb46bcc7f148a315335d96a6b
svdf18 <simonvandeurs@hotmail.com>
2023-01-31 13:37:54 +0100

    done w #
    done med id

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function sayHello() {
-    document.querySelector("h3").textContent = "Hello " + document.querySelector("#name").value;
-    document.querySelector("h4").textContent = document.querySelector("#age").value;
-    document.querySelector("h5").textContent = document.querySelector("#email").value;
+    document.querySelector("#namet").textContent = "Hello " + document.querySelector("#name").value + "!";
+    document.querySelector("#aget").textContent += "Age: " + document.querySelector("#age").value;
+    document.querySelector("#emailt").textContent += "Email: " + document.querySelector("#email").value;
 }
 
 document.querySelector("button").addEventListener("click", sayHello);
