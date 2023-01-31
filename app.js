def14a60645cd6d2981942562fb9bd1f79914d3c
svdf18 <simonvandeurs@hotmail.com>
2023-01-31 13:07:59 +0100

    inout update
    age + email

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 function sayHello() {
-    document.querySelector("h3").textContent = "Hello " + document.querySelector("input").value;
+    document.querySelector("h3").textContent = "Hello " + document.querySelector("#name").value;
+    document.querySelector("h4").textContent = document.querySelector("#age").value;
+    document.querySelector("h5").textContent = document.querySelector("#email").value;
 }
 
 document.querySelector("button").addEventListener("click", sayHello);
