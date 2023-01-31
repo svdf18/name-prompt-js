b1f67eacdd4ae53440ed501d056e98ef0a38965a
svdf18 <simonvandeurs@hotmail.com>
2023-01-31 12:41:46 +0100

    button action
    html input + button

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-let person = prompt("Please enter your name");
-
-if (person != null) {
-    document.body.innerHTML = ("Hello " + person + "! How are you today?");
+function sayHello() {
+    document.querySelector("h3").textContent = "Hello " + document.querySelector("input").value;
 }
 
-// trying to make a randomized, wholesome return consisting of two variables, one before and one after the prompt.
-
-// let person = prompt("Please enter your name");
-
-// if (person != null) {
-
-//     var befores = ['Hi', 'Hello', 'Buongiorno', 'Greetings'];
-//     var afters = ['! How are you today?', '! You are blessed and highly favoured', '! Let your kind light shine', '! This is your time for rest and relaxation']
-//     var before = befores[Math.floor(Math.random()*before.length)];
-//     var after = afters[Math.floor(Math.random()*after.length)];
-
-// document.body.innerHTML = (before + person + after);
-// }
-
-
+document.querySelector("button").addEventListener("click", sayHello);
